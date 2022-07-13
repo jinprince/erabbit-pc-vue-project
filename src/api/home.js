bbf209beee2fcc595fca7a3bca43ac9004dc78d7
mx-jin <2457376055@qq.com>
2022-07-13 22:29:36 +0800

    第三次提交

--- src/api/home.js
+++ src/api/home.js
@@ -2,3 +2,19 @@ import request from "@/utils/request"
 export const findBrand=(limit)=>{
     return request('/home/brand','get',{limit})
 }
+/**
+ * 获取广告图
+ * @returns Promise
+ */
+export const findBanner=()=>{
+    return request('/home/banner','get')
+}
+
+//新鲜好物
+export const findNew = () => {
+    return request('home/new', 'get')
+  }
+//   人气推荐
+  export const findHot = () => {
+    return request('home/hot', 'get')
+  }
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,15 +4,24 @@
       <div class="container">
         <!-- 左侧分类 -->
         <HomeCategory />
+        <!-- 轮播图 -->
+        <HomeBanner/>
+        <!-- 新鲜好物 -->
+        <HomeNew/>
+        <!-- 人气推荐 -->
+        <HomeHot/>
       </div>
     </div>
   </div>
 </template>
 <script>
 import HomeCategory from './components/home-category'
+import HomeBanner from './components/home-banner.vue'
+import HomeNew from './components/home-new.vue'
+import HomeHot from './components/home-hot.vue'
 export default {
   name: 'PageHome',
-  components: { HomeCategory }
+  components: { HomeCategory,HomeBanner,HomeNew,HomeHot }
 }
 </script>
 <style scoped lang="less"></style>
